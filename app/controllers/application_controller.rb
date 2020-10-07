58d824cc9e935707048aa7cb0d7f889f67c62716
Eugene Kawamoto <eugenenzv8@gmail.com>
2020-10-07 07:10:05 +0000

    Implove devise views & add header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # 後日admine側追加時に場合分け
+  def after_sign_in_path_for(resource)
+    public_root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    public_root_path
+  end
+
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email])
+  end
 end
