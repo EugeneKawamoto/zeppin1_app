3c599692270ea928b0974e8b05bbc07e00074b54
Eugene Kawamoto <eugenenzv8@gmail.com>
2020-10-07 05:42:50 +0000

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :public do
+    root to: 'reviews#index'
+    get 'homes/about' => 'homes#about'
+    get 'users/my_page' => 'users#my_page'
+    resources :users, only: [:edit, :update]
+    resources :reviews, only: [:new, :show, :create, :edit, :update, :destroy]
+    resources :menus, only: [:show]
+  end
 end
