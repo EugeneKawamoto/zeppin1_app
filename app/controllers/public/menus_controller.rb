ad8b471f84dd525318a9c39d39682bb3358cd563
Eugene Kawamoto <eugenenzv8@gmail.com>
2020-10-07 05:23:32 +0000

    add Controllers

---
+++ app/controllers/public/menus_controller.rb
@@ -0,0 +1,2 @@
+class Public::MenusController < ApplicationController
+end
---
+++ test/controllers/public/menus_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Public::MenusControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
